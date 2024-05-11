7f6996d35c0476c92ed4e252a59bc12752fbfb57
Vindy Aini <1998.vindy@gmail.com>
2024-05-11 16:57:25 +0700

    1

--- wp-content/plugins/elementor/assets/js/accordion.c16b88b2e8a0c50189bc.bundle.js
+++ wp-content/plugins/elementor/assets/js/accordion.c16b88b2e8a0c50189bc.bundle.js
@@ -1,4 +1,4 @@
-/*! elementor - v3.21.0 - 25-04-2024 */
+/*! elementor - v3.21.0 - 08-05-2024 */
 "use strict";
 (self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["accordion"],{
 
