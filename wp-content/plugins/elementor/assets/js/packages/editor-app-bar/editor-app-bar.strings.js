0a4d6e74a7a2140ff231b6829cc565b7789c4b97
Vindy Aini <1998.vindy@gmail.com>
2025-04-20 14:27:43 +0700

    1

--- wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
+++ wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
@@ -3,16 +3,16 @@ __( 'Help', 'elementor' );
 __( 'More', 'elementor' );
 __( 'Elementor Logo', 'elementor' );
 __( 'Integrations', 'elementor' );
-__( 'User Preferences', 'elementor' );
 __( 'Theme Builder', 'elementor' );
 __( 'Structure', 'elementor' );
 __( 'Site Settings', 'elementor' );
+__( 'Save Changes', 'elementor' );
 __( 'Switch Device', 'elementor' );
 // translators: %s: Breakpoint label, %d: Breakpoint size.
 __( '%s (%dpx and up)', 'elementor' );
 // translators: %s: Breakpoint label, %d: Breakpoint size.
 __( '%s (up to %dpx)', 'elementor' );
-__( 'Save Changes', 'elementor' );
+__( 'User Preferences', 'elementor' );
 __( 'Keyboard Shortcuts', 'elementor' );
 __( 'History', 'elementor' );
 __( 'Finder', 'elementor' );
